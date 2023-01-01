e42adfd92b199b188c424498431ef2f8a01b23e7
MOUNIN <2696660524@qq.com>
2023-01-01 17:12:39 +0800

    spring AspectJ配置文件（xml）

---
+++ src/com/lemon/spring1/aopxml/BookProxy.java
@@ -0,0 +1,8 @@
+package com.lemon.spring1.aopxml;
+
+public class BookProxy {
+
+    public void before() {
+        System.out.println("before.....");
+    }
+}
--- src/com/lemon/spring1/test/TestAop.java
+++ src/com/lemon/spring1/test/TestAop.java
@@ -1,6 +1,7 @@
 package com.lemon.spring1.test;
 
 import com.lemon.spring1.aopanno.User;
+import com.lemon.spring1.aopxml.Book;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -12,5 +13,12 @@ public class TestAop {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
         User user = context.getBean("user", User.class);
         user.add();
+    }
+
+    @Test
+    public void testAopXml() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
+        Book book = context.getBean("book", Book.class);
+        book.buy();
     }
 }
