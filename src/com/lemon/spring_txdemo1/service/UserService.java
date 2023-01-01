3fc151adfe531a6ade273c8124338f0413e32eac
MOUNIN <2696660524@qq.com>
2023-01-01 21:56:25 +0800

    spring 事务操作 隔离级别

--- src/com/lemon/spring_txdemo1/service/UserService.java
+++ src/com/lemon/spring_txdemo1/service/UserService.java
@@ -3,8 +3,12 @@ package com.lemon.spring_txdemo1.service;
 import com.lemon.spring_txdemo1.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ)
 public class UserService {
 
     // 注入dao
@@ -13,10 +17,27 @@ public class UserService {
 
     // 转账的方法
     public void accountMoney() {
-        // lucy少100
-        userDao.reduceMoney();
+        // 编程时方式事务管理
+//        try {
+//            // 第一步 开启事务
+//
+//            // 第二步 进行事务操作
+//            // lucy少100
+//            userDao.reduceMoney();
+//
+//            // mary多100
+//            userDao.addMoney();
+//
+//            // 第三步 没有发生异常，提交事务
+//        } catch (Exception e) {
+//            //第四步 出现异常，事务回滚
+//        }
+            // lucy少100
+            userDao.reduceMoney();
 
-        // mary多100
-        userDao.addMoney();
+            int i = 10 / 0;
+
+            // mary多100
+            userDao.addMoney();
     }
 }
