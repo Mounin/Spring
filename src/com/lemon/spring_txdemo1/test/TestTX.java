06aa393894793fa49a3ab0d1fdefe1afbe8e7a58
MOUNIN <2696660524@qq.com>
2023-01-02 17:28:24 +0800

    spring5 新功能 函数式注册对象

--- src/com/lemon/spring_txdemo1/test/TestTX.java
+++ src/com/lemon/spring_txdemo1/test/TestTX.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 public class TestTX {
 
@@ -21,5 +22,19 @@ public class TestTX {
         ApplicationContext context = new AnnotationConfigApplicationContext(TxConfig.class);
         UserService userService = context.getBean("userService", UserService.class);
         userService.accountMoney();
+    }
+
+    // 函数式风格创建对象，交给spring进行管理
+    @Test
+    public void testGenericApplicationContext() {
+        // 1. 创建GenericApplicationContext对象
+        GenericApplicationContext context = new GenericApplicationContext();
+        // 2. 调用context的方法的对象注册
+        context.refresh();
+        context.registerBean("user1", User.class, () -> new User());
+        // 3. 获取在spring注册的对象
+//        User user = (User)context.getBean("com.lemon.spring_txdemo1.test.User");
+        User user = (User)context.getBean("user1");
+        System.out.println(user);
     }
 }
---
+++ src/com/lemon/spring_txdemo1/test/User.java
@@ -0,0 +1,4 @@
+package com.lemon.spring_txdemo1.test;
+
+public class User {
+}
